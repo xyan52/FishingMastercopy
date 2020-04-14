9374e312efb8cc0b5d9295cc119fd07e0b03240c
xyan52 <xyan52@ucsc.edu>
2020-04-13 23:57:29 -0700

    initial commit

---
+++ prefabs/Rocket.js
@@ -0,0 +1,40 @@
+// Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // add object to existing scene
+        scene.add.existing(this);
+        // track firing status
+        this.isFiring = false; 
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
+    }
+
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >=47 ) {
+                this.x -= 2;
+            } else if (keyRIGHT.isDown && this.x <= 578) {
+                this.x += 2;
+            }
+        }
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play(); // play sfx
+        }
+        // if fired, move ip
+        if(this.isFiring && this.y >= 108) {
+            this.y -= 2;
+        }
+        // reset on miss
+        if(this.y <= 108) {
+            this.reset();
+        }
+    }
+    reset() {
+        this.isFiring = false;
+        this.y = 431;
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config);
+// define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000
+}
+// reserve keyboard vars
+let keyF, keyLEFT, keyRIGHT;
