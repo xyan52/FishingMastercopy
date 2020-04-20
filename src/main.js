9184b1bc8ab76a4887fb1672567b5cef1d164470
xyan52 <xyan52@ucsc.edu>
2020-04-19 23:26:40 -0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+/* 
+    xyan52
+    Sunny Yan
+    points breakdown:
+    Implement an alternating two-player mode (25)
+    Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (25)
+    Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (50)
+    Create your own mod and justify its score：worked hard (🤷🏻‍♂️)
+*/
 let config = {
     type: Phaser.CANVAS,
     width: 1280,
